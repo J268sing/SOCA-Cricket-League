618ae7518a10bcbc8b9de1c713a9ad5422960cbf
Jashandeep Singh <jashandeepsingh@Jashandeeps-MacBook-Pro.local>
2021-07-14 19:00:11 -0400

    Updates

---
+++ precache-manifest.3f0b5074c1a3d688bb1e1661b43f1327.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc20549808b10c6fbabfdbdd6d95e42",
+    "url": "/SOCA-Cricket-League/index.html"
+  },
+  {
+    "revision": "2c16ce13ee2cc3737d1e",
+    "url": "/SOCA-Cricket-League/static/css/main.a9732d3f.chunk.css"
+  },
+  {
+    "revision": "7ddadc4f9b59327d461c",
+    "url": "/SOCA-Cricket-League/static/js/2.6c0e02aa.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/SOCA-Cricket-League/static/js/2.6c0e02aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c16ce13ee2cc3737d1e",
+    "url": "/SOCA-Cricket-League/static/js/main.bd21ca22.chunk.js"
+  },
+  {
+    "revision": "bb77d1e79f19dbbb269c",
+    "url": "/SOCA-Cricket-League/static/js/runtime-main.b1815fb4.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/SOCA-Cricket-League/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "a2fd6288e227c74a20beec18221a62f6",
+    "url": "/SOCA-Cricket-League/static/media/dhruv.a2fd6288.jpg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/SOCA-Cricket-League/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/SOCA-Cricket-League/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/SOCA-Cricket-League/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/SOCA-Cricket-League/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/SOCA-Cricket-League/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/SOCA-Cricket-League/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/SOCA-Cricket-League/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/SOCA-Cricket-League/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/SOCA-Cricket-League/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/SOCA-Cricket-League/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "bbf2388f12132e73227206819f8f45d1",
+    "url": "/SOCA-Cricket-League/static/media/scorecard.bbf2388f.jpg"
+  },
+  {
+    "revision": "f9233bbfb9cdf9efddd3c82c1fe5937f",
+    "url": "/SOCA-Cricket-League/static/media/sharma.f9233bbf.jpg"
+  },
+  {
+    "revision": "e776a83448aa5d6ee2558009bb2aef71",
+    "url": "/SOCA-Cricket-League/static/media/sunrisephoto.e776a834.jpg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/SOCA-Cricket-League/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
